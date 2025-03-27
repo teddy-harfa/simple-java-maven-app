c9f71562b5519d3b065b59905ce986eabd8fe29d
teddy-harfa <teddyharfa@gmail.com>
2025-03-27 16:39:10 +0700

    Test deploy and run over ssh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -20,9 +20,9 @@ set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+# echo 'The following command runs and outputs the execution of your Java'
+# echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+# set -x
+# java -jar target/${NAME}-${VERSION}.jar
 
 
